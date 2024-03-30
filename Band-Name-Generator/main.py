9de2e42000496e4e74e50a1e341b34a67b98c15c
Camy.S1 <161379354+arrulost@users.noreply.github.com>
2024-03-30 11:54:06 +0200

    Add files via upload
     My first Python project: Band Name Generator. Get inspired with unique band names and join me on my coding journey!

---
+++ Band-Name-Generator/main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.")
+
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet name's?\n")
+
+print("Your band's name should be " + city_name + pet_name)
